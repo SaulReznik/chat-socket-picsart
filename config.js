a5fd5d5e831d7321efded7440d031d9567471b7c
SaulReznik <robhov97@gmail.com>
2020-11-21 23:22:50 +0400

    first remote commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.PORT = process.env.PORT || 3002;
+exports.PORT = 3002;
 exports.JWT_SECRET = '4-8-15-16-23-42';
 exports.DB_URI =
   'mongodb+srv://rob:chat-app@cluster0.kg6ha.mongodb.net/chat-app?retryWrites=true&w=majority';
