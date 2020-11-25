7e47f157a62feccdaa6359d61ff124c11838f528
SaulReznik <robhov97@gmail.com>
2020-11-25 15:56:13 +0400

    added credentials

--- index.js
+++ index.js
@@ -12,15 +12,9 @@ const app = express();
 const server = http.createServer(app);
 
 const io = socket(server, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
-      'Access-Control-Allow-Credentials': true
-    };
-    res.writeHead(200, headers);
-    res.end();
-  }
+  origin: true,
+  methods: ['GET', 'POST'],
+  credentials: true
 });
 
 //MongoDB connection
