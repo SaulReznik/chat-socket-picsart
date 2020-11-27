5af4624c14ddce0675f307eca076f85dc4c8e135
SaulReznik <robhov97@gmail.com>
2020-11-27 18:21:06 +0400

    added other middleware

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const io = socket(server, {
   cors: {
